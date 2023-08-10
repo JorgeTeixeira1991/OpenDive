b9b110e28f1dddb30544072c8a50b412bf3c9a17
Jorge Teixeira <jorge555444@gmail.com>
2023-08-10 15:52:52 +0100

    changes

---
+++ OpenDiveImageAPI/src/test/java/com/open/dive/OpenDiveImageAPI/OpenDiveImageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.dive.OpenDiveImageAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDiveImageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
