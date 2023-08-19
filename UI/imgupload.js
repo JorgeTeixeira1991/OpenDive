6e4e0ca98afc335223031c162857e97695dd77fa
Jorge Teixeira <jorge555444@gmail.com>
2023-08-19 22:43:42 +0100

    separating image api, basic tests successful

--- UI/imgupload.js
+++ UI/imgupload.js
@@ -1,83 +1,194 @@
-document.getElementById('uploadForm').addEventListener('submit', uploadImage);
+document.getElementById("uploadForm").addEventListener("submit", uploadImage);
+document.addEventListener("DOMContentLoaded", (event) => {
+  const savedImages = localStorage.getItem("base64Images");
+  if (savedImages) {
+    populateImages(JSON.parse(savedImages)); // Load from Local Storage
+  }
+});
 
 function uploadImage(event) {
+  event.preventDefault();
 
-    event.preventDefault(); // To prevent the form from submitting the default way
+  let fileInput = document.getElementById("fileInput");
+  let descInput = document.getElementById("desc");
 
-    let fileInput = document.getElementById('fileInput');
-    let descInput = document.getElementById('desc');
+  const file = fileInput.files[0];
 
-    console.log(fileInput.value)
-    console.log(descInput.value)
-    
+  // Resize the image first
+  resizeImage(file, function (resizedBlob) {
     const formData = new FormData();
-    formData.append('file', fileInput.files[0]);
-    formData.append('description', descInput.value);
-    
-    console.log(formData)
+    formData.append("file", resizedBlob); // Use the resized image blob
+    formData.append("description", descInput.value);
 
-    fetch('http://localhost:8080/api/image/upload', {
-      method: 'POST',
+    fetch("http://localhost:8080/api/image/upload", {
+      method: "POST",
       mode: "no-cors",
-      body: formData
+      body: formData,
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+}
 
-}   
+function resizeImage(file, callback) {
+  const reader = new FileReader();
 
-function fetchImage() {
+  reader.onload = function (event) {
+    const img = new Image();
+
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      const MAX_WIDTH = 800;
+      const scaleFactor = MAX_WIDTH / img.width;
+      canvas.width = MAX_WIDTH;
+      canvas.height = img.height * scaleFactor;
+
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      canvas.toBlob(function (blob) {
+        callback(blob);
+      }, file.type);
+    };
+
+    img.src = event.target.result;
+  };
+
+  reader.readAsDataURL(file);
+}
+
+/*function fetchImage() {
+
+
     let imageId = document.getElementById('imageId').value;
 
+
     if (imageId) {
+
         let imageUrl = `http://localhost:8080/api/image/${imageId}`;
+
         
         // Update the src attribute of the img tag
         document.getElementById('displayImage').src = imageUrl;
         document.getElementById('displayImage').style.width = '200px';
+        document.getElementById('displayImage').style.height = 'fit-content';
         document.getElementById('displayImage').style.margin = '10px';
+        document.getElementById('displayImage').style.display = '';
+        
 
     } else {
-        alert('Please enter an Image ID.');
+        document.getElementById('displayImage').style.display = 'none';
     }
-}
+}*/
+
+/*function fetchImages() {
+  const url = "http://localhost:8080/api/image";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((base64Images) => {
+      const imageContainer = document.getElementById("imageContainer");
+
+      // Clear previous images
+      imageContainer.innerHTML = "";
+      let imageId = 1;
+
+      // Append each image to the container
+      base64Images.forEach((base64Image) => {
+        let img = new Image(); // Create a new image element
+        img.src = "data:image/jpeg;base64," + base64Image;
+        img.style.width = "200px"; // Set width for better visualization, adjust as needed
+        img.style.margin = "10px"; // Add some margin between images
+
+        const linktoimage = document.createElement("a");
+        linktoimage.href = url.concat(`/img/${imageId}`);
+        console.log(imageId);
+        imageId = imageId + 1;
+
+        linktoimage.appendChild(img); // Append the image to the anchor element
+        imageContainer.appendChild(linktoimage); // Append the anchor element (which now contains the image) to the container
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching images:", error);
+    });
+}*/
 
 function fetchImages() {
-    fetch('http://localhost:8080/api/image')
-    .then(response => response.json())
-    .then(base64Images => {
-        const imageContainer = document.getElementById('imageContainer');
-        
-        // Clear previous images
-        imageContainer.innerHTML = '';
+  const url = "http://localhost:8080/api/image";
 
-        // Append each image to the container
-        base64Images.forEach(base64Image => {
-            const img = document.createElement('img');
-            img.src = 'data:image/jpeg;base64,' + base64Image;
-            img.style.width = '200px';  // Set width for better visualization, adjust as needed
-            img.style.margin = '10px';  // Add some margin between images
-            imageContainer.appendChild(img);
-        });
+  fetch(url)
+    .then((response) => response.json())
+    .then((base64Images) => {
+      localStorage.setItem("base64Images", JSON.stringify(base64Images)); // Save to Local Storage
+      populateImages(base64Images);
     })
-    .catch(error => {
-        console.error('Error fetching images:', error);
+    .catch((error) => {
+      console.error("Error fetching images:", error);
     });
 }
-function unloadImages() {
-    const imageContainer = document.getElementById('imageContainer');
-    imageContainer.innerHTML = '';  // Clear the contents of the container
+
+function populateImages(base64Images) {
+  const imageContainer = document.getElementById("imageContainer");
+
+  // Clear previous images
+  imageContainer.innerHTML = "";
+  let imageId = 1;
+
+  // Append each image to the container
+  base64Images.forEach((base64Image) => {
+    let img = new Image();
+    img.src = "data:image/jpeg;base64," + base64Image;
+    img.style.width = "200px";
+    img.style.margin = "10px";
+
+    const linktoimage = document.createElement("a");
+    linktoimage.href = "http://localhost:8080/api/image".concat(
+      `/img/${imageId}`
+    );
+    console.log(imageId);
+    imageId = imageId + 1;
+
+    linktoimage.appendChild(img);
+    imageContainer.appendChild(linktoimage);
+  });
 }
 
+function fetchImage() {
+  const imageId = document.getElementById("imageId").value;
+  const imageContainer = document.getElementById("displayImage");
 
+  fetch(`http://localhost:8080/api/image/${imageId}`)
+    .then((response) => {
+      // Check if the status is 200
+      if (response.status !== 200) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log(` Success! Status: ${response.status}`);
+      return response.json();
+    })
+    .then((base64Image) => {
+      imageContainer.innerHTML = "";
+      const img = document.createElement("img");
+      img.src = "data:image/jpeg;base64," + base64Image;
+      img.style.width = "200px"; // Set width for better visualization, adjust as needed
+      img.style.margin = "10px"; // Add some margin between images
+      imageContainer.appendChild(img);
+    })
+    .catch((error) => {
+      console.error("Error fetching images:", error);
+      imageContainer.innerHTML = "";
+    });
+}
 
-
-
-
-
-
+function unloadImages() {
+  let imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = ""; // Clear the contents of the container
+  localStorage.removeItem("base64Images");
+}
--- UI/index.js
+++ UI/index.js
@@ -1,65 +1,72 @@
-window.onload = function() {
+window.onload = function () {
+  fillSectionWithContent();
+};
+
+function createCard(imageUrl, title, description, link) {
+  // Create the elements
+  var card = document.createElement("div");
+  var img_frame = document.createElement("div");
+  var image = document.createElement("img");
+  var cardTitle = document.createElement("h2");
+  var cardDescription = document.createElement("p");
+  var seeMoreLink = document.createElement("a");
+
+  card.classList.add("card");
+
+  // Set the attributes and content
+
+  img_frame.style.overflow = "hidden";
+  img_frame.style.width = "200px";
+  img_frame.style.height = "200px";
+  img_frame.style.display = "flex";
+  img_frame.style.justifyContent = "center";
+  img_frame.style.alignItems = "center";
+  image.src = imageUrl;
+  image.style.height = "200px";
+  image.style.width = "fit-content";
+  cardTitle.textContent = title;
+  cardDescription.textContent = description;
+  seeMoreLink.textContent = "See More";
+  seeMoreLink.href = link;
+
+  // Append the elements to the card
+  img_frame.appendChild(image);
+  card.appendChild(img_frame);
+  card.appendChild(cardTitle);
+  card.appendChild(cardDescription);
+  card.appendChild(seeMoreLink);
+
+  // Append the card to the section
+  var section = document.getElementById("cardSection");
+  section.appendChild(card);
+}
+
+function fillSectionWithContent() {
+  // Define the desired number of cards to fill the visible section
+  var cardsPerSection = 7;
+
+  for (var i = 1; i < cardsPerSection; i++) {
+    // Generate the card content dynamically
+    var imageUrl = `http://localhost:8080/api/image/img/${i}`;
+    var title = "Dish Title ";
+    var description = "This is the description of Card " + i;
+    var link = `http://localhost:8080/api/image/img/${i}`;
+
+    // Create and append the card
+    createCard(imageUrl, title, description, link);
+  }
+}
+
+// Load more content as you scroll down
+window.addEventListener("scroll", function () {
+  // Calculate the scroll position, visible height, and full height of the document
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  var windowHeight = window.innerHeight;
+  var fullHeight = document.documentElement.offsetHeight;
+
+  // Determine if the user has scrolled to the bottom of the visible section
+  if (scrollPosition + windowHeight >= fullHeight) {
+    // Load more content
     fillSectionWithContent();
-  };
-  
-  function createCard(imageUrl, title, description, link) {
-    // Create the elements
-    var card = document.createElement('div');
-    var image = document.createElement('img');
-    var cardTitle = document.createElement('h2');
-    var cardDescription = document.createElement('p');
-    var seeMoreLink = document.createElement('a');
-
-    card.classList.add('card');
-  
-    // Set the attributes and content
-    image.src = imageUrl;
-    cardTitle.textContent = title;
-    cardDescription.textContent = description;
-    seeMoreLink.textContent = 'See More';
-    seeMoreLink.href = link;
-  
-    // Append the elements to the card
-    card.appendChild(image);
-    card.appendChild(cardTitle);
-    card.appendChild(cardDescription);
-    card.appendChild(seeMoreLink);
-  
-    // Append the card to the section
-    var section = document.getElementById('cardSection');
-    section.appendChild(card);
   }
-  
-  function fillSectionWithContent() {
-    // Define the desired number of cards to fill the visible section
-    var cardsPerSection = 6;
-  
-    for (var i = 0; i < cardsPerSection; i++) {
-      // Generate the card content dynamically
-      var imageUrl = 'https://picsum.photos/200';
-      var title = 'Card Title ' + (i +1);
-      var description = 'This is the description of Card ' + i;
-      var link = 'https://example.com/' + i;
-  
-      // Create and append the card
-      createCard(imageUrl, title, description, link);
-    }
-  }
-  
-  // Load more content as you scroll down
-  window.addEventListener('scroll', function() {
-    // Calculate the scroll position, visible height, and full height of the document
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    var windowHeight = window.innerHeight;
-    var fullHeight = document.documentElement.offsetHeight;
-  
-    // Determine if the user has scrolled to the bottom of the visible section
-    if (scrollPosition + windowHeight >= fullHeight) {
-      // Load more content
-      fillSectionWithContent();
-    }
-  });
-  
-  
-
-  
+});
