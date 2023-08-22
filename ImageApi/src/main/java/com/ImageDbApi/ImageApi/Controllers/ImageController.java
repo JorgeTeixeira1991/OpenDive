9cab985ddef7ef536b501b7c9c41c01d61f8687e
Jorge Teixeira <jorge555444@gmail.com>
2023-08-22 19:45:56 +0100

    dunno

--- ImageApi/src/main/java/com/ImageDbApi/ImageApi/Controllers/ImageController.java
+++ ImageApi/src/main/java/com/ImageDbApi/ImageApi/Controllers/ImageController.java
@@ -17,7 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
-  @Autowired private ImageService imageService;
+  @Autowired
+  private ImageService imageService;
 
   @PostMapping("/upload")
   public ResponseEntity<String> uploadImage(
@@ -64,7 +65,7 @@ public class ImageController {
     }
 
     for (Image i : imageList) {
-      String base64Image = Base64.getEncoder().encodeToString(i.getData());
+      String base64Image = "[\"" + Base64.getEncoder().encodeToString(i.getData()) + "\"]";
       base64Images.add(base64Image);
     }
 
